7d390230cdb2b01397c1aeaf3560a1b50a7234ff
hatpoppo <toshi.hat@gmail.com>
2025-12-08 17:01:03 +0900

    Ziggyのrouteメソッドに対する対応

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,5 @@
-<script setup>
-import { Head, Link } from '@inertiajs/vue3';
-
+<script setup lang="ts">
+import { Head, Link } from "@inertiajs/vue3";
 defineProps({
     canLogin: {
         type: Boolean,
@@ -19,10 +18,10 @@ defineProps({
 });
 
 function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
+    document.getElementById("screenshot-container")?.classList.add("!hidden");
+    document.getElementById("docs-card")?.classList.add("!row-span-1");
+    document.getElementById("docs-card-content")?.classList.add("!flex-row");
+    document.getElementById("background")?.classList.add("!hidden");
 }
 </script>
 
---
+++ resources/js/types/index.d.ts
@@ -0,0 +1,8 @@
+import { route as routeFn } from "ziggy-js";
+
+declare module "vue" {
+    interface ComponentCustomProperties {
+        route: typeof routeFn;
+        $page: Page;
+    }
+}
